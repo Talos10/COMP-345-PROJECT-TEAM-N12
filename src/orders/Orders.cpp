d0d69f3c7a2f8e970934398755cf0ea6fac3f7c4
d-rossi <david.rossi101@gmail.com>
2021-10-04 14:01:02 -0400

    Added comments to Orders.h and Orders.cpp

--- src/orders/Orders.cpp
+++ src/orders/Orders.cpp
@@ -1,65 +1,87 @@
 #include "Orders.h"
 
 ////////////////////////////Order CLASS////////////////////////////////////
+//Default constructor
 Order::Order() : Order("generic order", "no effect") {}
+
+//Parameterized constructor which initializes an Order with the provided description and effect
 Order::Order(string description, string effect) {
     this->description = new string(description);
     this->effect = new string(effect);
 }
+
+//Copy constructor
 Order::Order(const Order& order) {
     this->description = new string(*(order.description));
     this->effect = new string(*(order.effect));
-
 }
+
+//Destructor
 Order::~Order() {
     delete description;
     delete effect;
 }
 
+//Getter to retrieve the description of an Order
 string Order::getDescription() const {
     return *(this->description);
 }
 
+//Getter to retrieve the effect of an Order
 string Order::getEffect() const {
     return *(this->effect);
 }
 
+//Setter to set the description of an Order
 void Order::setDescription(const string& description) {
     *(this->description) = description;
 }
 
+//Setter to set the effect of an Order
 void Order::setEffect(const string& effect) {
     *(this->effect) = effect;
 }
 
+//Defining the equality operator
 Order& Order::operator=(const Order& order) {
     this->description = new string(*(order.description));
     this->effect = new string(*(order.effect));
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Order& order) {
 	out << *(order.description);
 	return out;
 }
 
+////Checks if an order is valid
 bool Order::validate() {
     return true;
 }
 
+//Executes an order if it is valid.
 void Order::execute() {
     cout << "executing" << endl;
 }
 
 ////////////////////////////Deploy CLASS////////////////////////////////////
+//Default constructor
 Deploy::Deploy(): Order("Deploy Default", "Deploy effect") {}
+
+//Copy constructor
 Deploy::Deploy(const Deploy& deploy_order): Order(deploy_order) {}
+
+//Destructor
 Deploy::~Deploy() {};
+
+//Checks if a Deploy order is valid
 bool Deploy::validate() {
     cout << "Validating Deploy Order" << endl;
     return true;
 }
 
+//Executes a Deploy order
 void Deploy::execute() {
     if (this->validate()) {
         cout << "Executing Deploy Order" << endl;
@@ -70,25 +92,35 @@ void Deploy::execute() {
     }
 }
 
+//Defining the equality operator
 Deploy& Deploy::operator=(const Deploy& deploy_order) {
     Order::operator=(deploy_order);
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Deploy& deploy) {
 	out << deploy.getDescription();
 	return out;
 }
 
 ////////////////////////////Advance CLASS////////////////////////////////////
+//Default constructor
 Advance::Advance(): Order("Advance Default", "Advance effect") {}
+
+//Copy constructor
 Advance::Advance(const Advance& adv_order): Order(adv_order) {}
+
+//Desctructor
 Advance::~Advance() {};
+
+//Checks if an Advance order is valid
 bool Advance::validate() {
     cout << "Validating Advance Order" << endl;
     return true;
 }
 
+//Executes an Advance order
 void Advance::execute() {
     if (this->validate()) {
         cout << "Executing Advance Order" << endl;
@@ -99,25 +131,35 @@ void Advance::execute() {
     }
 }
 
+//Defining the equality operator
 Advance& Advance::operator=(const Advance& adv_order) {
     Order::operator=(adv_order);
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Advance& advance) {
 	out << advance.getDescription();
 	return out;
 }
 
 ////////////////////////////Bomb CLASS////////////////////////////////////
+//Default constructor
 Bomb::Bomb(): Order("Bomb Default", "Bomb effect") {}
+
+//Copy constructor
 Bomb::Bomb(const Bomb& bomb_order): Order(bomb_order) {}
+
+//Destructor
 Bomb::~Bomb() {};
+
+//Checks if a Bomb order is valid
 bool Bomb::validate() {
     cout << "Validating Bomb Order" << endl;
     return true;
 }
 
+//Executes a Bomb order
 void Bomb::execute() {
     if (this->validate()) {
         cout << "Executing Bomb Order" << endl;
@@ -128,25 +170,35 @@ void Bomb::execute() {
     }
 }
 
+//Defining the equality operator
 Bomb& Bomb::operator=(const Bomb& bomb_order) {
     Order::operator=(bomb_order);
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Bomb& bomb) {
 	out << bomb.getDescription();
 	return out;
 }
 
 ////////////////////////////Blockade CLASS////////////////////////////////////
+//Default constructor
 Blockade::Blockade(): Order("Blockade Default", "Blockade effect") {}
+
+//Copy constructor
 Blockade::Blockade(const Blockade& blockade_order): Order(blockade_order) {}
+
+//Destructor
 Blockade::~Blockade() {};
+
+//Checks if a Blockade order is valid
 bool Blockade::validate() {
     cout << "Validating Blockade Order" << endl;
     return true;
 }
 
+//Executes a Blockade order
 void Blockade::execute() {
     if (this->validate()) {
         cout << "Executing Blockcade Order" << endl;
@@ -158,25 +210,35 @@ void Blockade::execute() {
     
 }
 
+//Defining the equality operator
 Blockade& Blockade::operator=(const Blockade& blockade_order) {
     Order::operator=(blockade_order);
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Blockade& blockade) {
 	out << blockade.getDescription();
 	return out;
 }
 
 ////////////////////////////Airlift CLASS////////////////////////////////////
+//Default constructor
 Airlift::Airlift(): Order("Airlift Default", "Airlift effect") {}
+
+//Copy constructor
 Airlift::Airlift(const Airlift& airlift_order): Order(airlift_order) {}
+
+//Destructor
 Airlift::~Airlift() {};
+
+//Checks if an Airlift order is valid
 bool Airlift::validate() {
     cout << "Validating Airlift Order" << endl;
     return true;
 }
 
+//Executes an Airlift order
 void Airlift::execute() {
     if (this->validate()) {
         cout << "Executing Airlift Order" << endl;
@@ -187,55 +249,73 @@ void Airlift::execute() {
     }
 }
 
+//Defining the equality operator
 Airlift& Airlift::operator=(const Airlift& airlift_order) {
     Order::operator=(airlift_order);
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Airlift& airlift) {
 	out << airlift.getDescription();
 	return out;
 }
 
 ////////////////////////////Negotiate CLASS////////////////////////////////////
+//Default constructor
 Negotiate::Negotiate(): Order("Negotiate Default", "Negotiate effect") {}
+
+//Copy constructor
 Negotiate::Negotiate(const Negotiate& negotiate_order): Order(negotiate_order) {}
+
+//Destructor
 Negotiate::~Negotiate() {};
+
+//Checks if a Negotiate order is valid
 bool Negotiate::validate() {
     cout << "Validating Negotiate Order" << endl;
     return true;
 }
 
+//Executes a Negotiate order
 void Negotiate::execute() {
     cout << "Executing Negotiate Order" << endl;
 }
 
+//Defining the equality operator
 Negotiate& Negotiate::operator=(const Negotiate& negotiate_order) {
     Order::operator=(negotiate_order);
     return *this;
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const Negotiate& negotiate) {
 	out << negotiate.getDescription();
 	return out;
 }
 
 ////////////////////////////OrdersList CLASS////////////////////////////////////
+//Default constructor
 OrdersList::OrdersList() {
     this->orders = new vector<Order>();
 }
+
+//Parameterized constructor which instantiates the vector of orders with the provided vector of orders
 OrdersList::OrdersList(vector<Order>& orders) {
     this->orders = new vector<Order>(orders);
 }
 
+//Copy constructor
 OrdersList::OrdersList(const OrdersList& o_list) {
     this->orders = new vector<Order>(*o_list.orders);
 }
 
+//Destructor
 OrdersList::~OrdersList() {
     delete orders;  //might be wrong???
 } 
 
+//Move an Order in the vector to a new index by providing its current index and the index it should be moved to
 void OrdersList::move(int currentIndex, int newIndex) {
     int order_list_size = this->orders->size();
     if (currentIndex < order_list_size && newIndex < order_list_size && currentIndex >= 0 && newIndex >= 0) {
@@ -248,6 +328,7 @@ void OrdersList::move(int currentIndex, int newIndex) {
     }
 }
 
+//Remove an order in the vector by specifying its respective index
 void OrdersList::remove(const int orderIndex) {
     if (orderIndex < this->orders->size()) {
         this->orders->erase(this->orders->begin() + orderIndex);
@@ -257,27 +338,33 @@ void OrdersList::remove(const int orderIndex) {
     }
 }
 
+//Getter for the vector of orders
 vector<Order>& OrdersList::getOrders() const {
     return *(this->orders);
 }
 
+//Add an order to the vector by providing an Order
 void OrdersList::addOrder(const Order& order) {
     this->orders->push_back(order);
 }
 
+//Add an order to the vector at a specific index by providing an Order and the desired index position
 void OrdersList::addOrder(const Order& order, int index) {
         this->orders->insert(this->orders->begin() + index, order);
 }
 
+//Defining the equality operator
 OrdersList& OrdersList::operator=(const OrdersList& ordersList) {
     this->orders = new vector<Order>(*(ordersList.orders));
     return *this;
 }
 
+//Defining the addition operator
 void OrdersList::operator+(Order& order) {
     this->addOrder(order);
 }
 
+//Defining the output operator
 ostream& operator<<(ostream& out, const OrdersList& ol) {
 	out << "The OrdersList contains " << ol.orders->size() << " orders:" << endl;
     for (Order order : *(ol.orders)) {
--- src/orders/OrdersDriver.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Running orders driver!" << std::endl;
-    return 0;
-}
