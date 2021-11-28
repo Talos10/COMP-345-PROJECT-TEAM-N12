3e3b35706d0c8c44d885b175ac455d7b2b61d590
unknown <sustaccosmin544@hotmail.ca>
2021-11-28 18:15:38 -0500

    Implement benevolent player

--- src/player_strategy/PlayerStrategies.h
+++ src/player_strategy/PlayerStrategies.h
@@ -118,6 +118,9 @@ public:
 
     // toDefend method for the benevolent player
     vector<tuple<Territory*,Territory*,string>> toDefend(Player* player) override;
+
+    //Checks if all neighbors of a territory belong to the same player (owner)
+    bool checkIfAllNeighborsBelongToSamePlayer(Territory* terr);
 };
 
 class NeutralPlayerStrategy : public PlayerStrategy {
